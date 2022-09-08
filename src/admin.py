dfc076d6a2d136540c84ccc951b62e23c7c82238
angelicamaliqueo <angelicamaliqueor@gmail.com>
2022-09-08 05:46:17 +0000

    trabajo subido

--- src/admin.py
+++ src/admin.py
@@ -11,6 +11,12 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(People, ds.session))
+    admin.add_view(ModelView(Planets, ds.session))
+    admin.add_view(ModelView(Starship, ds.session))
+    admin.add_view(ModelView(Favorite_people, ds.session))
+    admin.add_view(ModelView(Favorite_planets, ds.session))
+    admin.add_view(ModelView(Favorite_starships, ds.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
